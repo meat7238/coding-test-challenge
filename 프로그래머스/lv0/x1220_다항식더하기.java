b15366ad31964f496bb0b76ca615ac9d1895ff87
Meat7238 <meat7238@kakao.com>
2022-12-20 18:34:27 +0900

    221220

---
+++ 프로그래머스/lv0/x1220_다항식더하기.java
@@ -0,0 +1,23 @@
+package 프로그래머스.lv0;
+
+public class x1220_다항식더하기 {
+    public static void main(String[] args) {
+        String polynomial = "3x + 7 + x";
+        String[] strs = polynomial.replace(" +","").split(" +");
+        int a = 0;
+        int b = 0;
+        for(String s : strs){
+            if(s.contains("x")) {
+                if(s == "x") {
+                    a+=1;
+                    break;
+                }
+                a+=Integer.parseInt(s.replace("x",""));
+                continue;
+            }
+            b += Integer.parseInt(s);
+        }
+        System.out.println(a + " " + b);
+//        String answer = a+"x"+" "+b;
+    }
+}
--- 프로그래머스/lv1/x1011_완주하지못한선수.java
+++ 프로그래머스/lv1/x1011_완주하지못한선수.java
@@ -5,29 +5,45 @@ import java.util.Map;
 
 public class x1011_완주하지못한선수 {
 	public static void main(String[] args) {
-	Solution1011 s = new Solution1011();
+//	Solution1011 s = new Solution1011();
+		Solution1220 s = new Solution1220();
 	String[] participant = {"mislav","stanko","mislav","ana"};
 	String[] completion = {"stanko","ana","mislav"};
 	System.out.println(s.solution(participant, completion));
 	}
 }
 
-class Solution1011 {
-    public String solution(String[] participant, String[] completion) {
-		HashMap<String, Integer> map = new HashMap<>();
-		
-		for(String s : participant)	map.put(s, map.getOrDefault(s, 0)+1);
-		for(String s : completion) map.put(s, map.get(s)-1);
-		
-		
-		String answer = "";
-		for(Map.Entry<String, Integer> entry : map.entrySet()) {
-			if(entry.getValue()>0) {
-				answer += entry.getKey();
-				break;
+//class Solution1011 {
+//    public String solution(String[] participant, String[] completion) {
+//		HashMap<String, Integer> map = new HashMap<>();
+//
+//		for(String s : participant)	map.put(s, map.getOrDefault(s, 0)+1);
+//		for(String s : completion) map.put(s, map.get(s)-1);
+//
+//
+//		String answer = "";
+//		for(Map.Entry<String, Integer> entry : map.entrySet()) {
+//			if(entry.getValue()>0) {
+//				answer += entry.getKey();
+//				break;
+//			}
+//
+//		}
+//        return answer;
+//    }
+//}
+
+	class Solution1220 {
+		public String solution(String[] participant, String[] completion) {
+			HashMap<String, Integer> map = new HashMap<>();
+			for(String s : participant){
+				map.put(s, map.getOrDefault(s,0)+1);
 			}
-			
+			for(Map.Entry<String,Integer> entry : map.entrySet() ){
+				System.out.println(entry.getKey() + " / " + entry.getValue());
+			}
+			for(String s : completion){
+			}
+			return "";
 		}
-        return answer;
-    }
-}
+	}
