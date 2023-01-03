5986a33d6900ea58ffadcf0db61be6d05f0fb6ce
Meat7238 <meat7238@kakao.com>
2023-01-03 21:31:42 +0900

    230103

--- 프로그래머스/lv0/x1220_다항식더하기.java
+++ 프로그래머스/lv0/x1220_다항식더하기.java
@@ -1,23 +1,24 @@
-//package 프로그래머스.lv0;
-//
-//public class x1220_다항식더하기 {
-//    public static void main(String[] args) {
-//        String polynomial = "3x + 7 + x";
-//        String[] strs = polynomial.replace(" +","").split(" +");
-//        int a = 0;
-//        int b = 0;
-//        for(String s : strs){
-//            if(s.contains("x")) {
-//                if(s == "x")
-//                    a+=1;
-//                    break;
-//                }
-//                a+=Integer.parseInt(s.replace("x",""));
-//                continue;
-//            }
-//            b += Integer.parseInt(s);
-//        }
-//        System.out.println(a + " " + b);
-//        String answer = a+"x"+" "+b;
-//    }
-//}
+package 프로그래머스.lv0;
+
+import java.util.Arrays;
+
+public class x1220_다항식더하기 {
+    public static void main(String[] args) {
+        String polynomial = "x + x + x";
+        int a = 0;   // 계수
+        int b = 0;   // 상수
+        for (String p : polynomial.split(" ")) {
+            if (p.contains("x")) a += p.equals("x") ? 1 : Integer.parseInt(p.substring(0, p.length() - 1));
+            else if (!p.equals("+")) b += Integer.parseInt(p);
+        }
+        String coefStr = a > 0 ? a == 1 ? "x" : a + "x" : "";
+        String consStr = b > 0 ? String.valueOf(b) : "";
+        String result = "";
+        if (a > 0) {
+            if (b > 0) result += coefStr + " + " + consStr;
+            else result += coefStr;
+        }
+        else if (b > 0) result += consStr;
+        System.out.println(result);
+    }
+}
